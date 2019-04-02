351fe96f7a205039be23186fa62737e1843b4575
SergioGOU <49206432+SergioGOU@users.noreply.github.com>
2019-04-02 14:26:15 -0600

    Add files via upload

---
+++ PockemonS/PokemonS/pokemons.swift
@@ -0,0 +1,28 @@
+﻿//
+//  pokemon.swift
+//  Pokemon
+//
+//  Created by Isabel Gomez on 3/6/19.
+//  Copyright © 2019 Sergio Gomez. All rights reserved.
+//
+
+import UIKit
+
+class pokemons{
+    var latitude:Double?
+    var longitude:Double?
+    var image:String?
+    var name:String?
+    var des:String?
+    var power:Double?
+    var isCatch:Bool?
+    init(latitude:Double,longitude:Double,image:String,name:String,des:String,power:Double){
+        self.latitude=latitude
+        self.longitude=longitude
+        self.image=image
+        self.name=name
+        self.des=des
+        self.power=power
+        self.isCatch=false
+    }
+}
